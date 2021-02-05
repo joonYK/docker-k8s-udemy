a576ff2e0435626a7f133ba24745ec705502a60c
JY <kims2606@gmail.com>
2021-02-05 09:08:06 +0900

    data 및 volume 작업을 위한 node 앱

---
+++ 3. Data 관리 및 Volume 작업/data-volumes-01-starting-setup/server.js
@@ -0,0 +1,54 @@
+const fs = require('fs').promises;
+const exists = require('fs').exists;
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+//feedback폴더 접근 가능
+app.use('/feedback', express.static('feedback'));
+
+//get feedback.html
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'feedback.html');
+  res.sendFile(filePath);
+});
+
+//get exists.html
+app.get('/exists', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'exists.html');
+  res.sendFile(filePath);
+});
+
+//파일을 저장
+app.post('/create', async (req, res) => {
+  //제목
+  const title = req.body.title;
+  //내용
+  const content = req.body.text;
+
+  const adjTitle = title.toLowerCase();
+
+  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
+  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
+
+  //temp폴더에 파일을 미리 저장
+  await fs.writeFile(tempFilePath, content);
+
+  //같은 제목의 파일이 feedback폴더에 없으면 temp폴더에 있던 파일을 feedback폴더로 저장
+  exists(finalFilePath, async (exists) => {
+    if (exists) {
+      res.redirect('/exists');
+    } else {
+      await fs.rename(tempFilePath, finalFilePath);
+      res.redirect('/');
+    }
+  });
+});
+
+app.listen(80);
